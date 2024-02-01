352bd3293253a092b617518f12d6a4207c22e290
Daniela Rus Morales <danirus@eml.cc>
2024-02-01 10:08:34 +0100

    Compat with Django 5.0. Update deps.

--- project_stories/project_stories/enums.py
+++ project_stories/project_stories/enums.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from django_comments_ink.models import BaseReactionEnum
 
 
--- project_stories/users/utils.py
+++ project_stories/users/utils.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.template import loader
 from django.urls import reverse
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from django_comments_ink.utils import send_mail
 
 
