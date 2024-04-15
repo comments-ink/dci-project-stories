24deab99525e0d8bed989bad8e4bf75d02863afc
Matteo Gamboz <gamboz@medialab.sissa.it>
2024-04-15 11:15:29 +0200

    use template list_reacted_to_object.html
    but call it users_reacted_to_object.html, as it's referenced in
    django-comments-ink.views.reacting.ReactedToObjectUserListView

--- project_stories/static/modal.js
+++ project_stories/static/modal.js
@@ -65,16 +65,9 @@ const init_modals = function() {
         const response = await fetch(trigger.dataset.href, {method: 'GET'});
         if (response.status === 200) {
             const data = await response.text();
-
-            // Extract the <main> element from the response text
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, "text/html");
-            const mainElement = doc.querySelector("main");
-            const mainHtml = mainElement.outerHTML;
-
             const body = target && target.querySelector(".modal__wrapper");
             if (body) {
-                body.innerHTML = mainHtml;
+                body.innerHTML = data;
                 addModalListeners();
             }
         }
